35249601f4b4b183f29bb666b5a5ef03d7116e63
Harrison <harrison.hall23@d155.org>
2023-02-28 09:43:05 -0600

    Initial Commit - Basics setup

--- BaseballStealBases/ViewController.swift
+++ BaseballStealBases/ViewController.swift
@@ -7,13 +7,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CustomTableViewCell: UITableViewCell {
+  
+    
+    
+}
 
+class ViewController: UIViewController {
+    @IBOutlet weak var poptimeInput: UITextField!
+    @IBOutlet weak var pitchSpeedInput: UILabel!
+    
+    @IBOutlet weak var playerNameInput: UITextField!
+    @IBOutlet weak var playerBasetimeInput: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func addPlayerAction(_ sender: UIButton) {
+        // Need firebase to save player info
+    }
+    
 }
 
